8306b08dfda3d51350b09a03105fed00634441c7
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2023-12-06 17:16:31 +0100

    Start writing algorithm
    Identified main steps of the program and as Input Player, Computer Choice,
    Play Round and Output Result and started breaking those down further.

---
+++ javascript.js
@@ -0,0 +1,34 @@
+//USER INPUT
+//Prompt user to input a string saying either 'Rock', 'Paper' or 'Scissors'
+//Save the user choice in a variable playerSelection
+//Set the user choice string to lowercase
+
+
+//COMPUTER CHOICE
+
+//Declare a variable computerSelection
+//Choose a random option of either 'rock', 'paper', or 'scissors' <-- How?
+//Save that option in computerSelection
+
+//PLAY ROUND
+
+//Evaluate the user choice against the computer choice
+//If playerSelection is 'rock' and computerSelection is
+//    'rock'     --> tie
+//    'paper'    --> loss
+//    'scissors' --> win
+//If playerSelection is 'paper' and computerSelection is
+//    'rock'     --> win
+//    'paper'    --> tie
+//    'scissors' --> loss
+////If playerSelection is 'scissors' and computerSelection is
+//    'rock'     --> loss
+//    'paper'    --> win
+//    'scissors' --> tie
+//Save string of 'win', 'loss', or 'tie' in variable roundResult 
+
+//OUTPUT
+
+//Alert message according to win, loss, or tie 
+//If result was a tie, display message 'Let's try again!'
+//Prompt user to try again.
