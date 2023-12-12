69e82d42182a76981288530dfd1dd608eda10312
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2023-12-12 18:00:41 +0100

    Create function and variable for computer selection I found something about arrays that gets you a random choice from among different strings. Thought about using that to create the computer's choice. However, I haven't learned about arrays yet and I also wanted to understand how to create a random number in JavaScript because, it seems like a pretty versatile approach to all kinds of problems that involve creating random variables, so I went with this option.

--- javascript.js
+++ javascript.js
@@ -3,14 +3,40 @@
 //Save the user choice in a variable playerSelection
 //Set the user choice string to lowercase
 
-const playerSelection = prompt("Please choose 'Rock', 'Paper', or 'Scissors'!").toLowerCase();
+//const playerSelection = prompt("Please choose 'Rock', 'Paper', or 'Scissors'!").toLowerCase();
 
 
 //COMPUTER CHOICE
 
 //Declare a variable computerSelection
-//Choose a random option of either 'rock', 'paper', or 'scissors' <-- How?
+//Choose a random option of either 'rock', 'paper', or 'scissors' <-- How? ==> Random number generator
+// generate a random number between 1 and 3
+    //const min = 1;
+    //const max = 3;
+    //const random = Math.floor(Math.random() * (max - min + 1)) + min;
+    //match numbers to different return values
 //Save that option in computerSelection
+function getComputerChoice () {
+    
+    const random = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    switch (random) {
+        case 1:
+            return 'rock';
+            break;
+        case 2:
+            return 'paper';
+            break;
+        case 3:
+                return 'scissors';
+                break;
+
+    }
+}
+
+let computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+
 
 //PLAY ROUND
 
@@ -32,5 +58,6 @@ const playerSelection = prompt("Please choose 'Rock', 'Paper', or 'Scissors'!").
 //OUTPUT
 
 //Alert message according to win, loss, or tie 
+//Remember to use template literals to include player and computer choices
 //If result was a tie, display message 'Let's try again!'
 //Prompt user to try again.
