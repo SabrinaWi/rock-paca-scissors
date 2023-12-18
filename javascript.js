710676e88ac2dcd65ae60e515b529feec68cb63a
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2023-12-18 20:35:43 +0100

    Create playRound function
    Create function to play one round and console.log() results of
    playerSelection, computerSelection and playRound to test if it runs right.

--- javascript.js
+++ javascript.js
@@ -3,8 +3,8 @@
 //Save the user choice in a variable playerSelection
 //Set the user choice string to lowercase
 
-//const playerSelection = prompt("Please choose 'Rock', 'Paper', or 'Scissors'!").toLowerCase();
-
+const playerSelection = prompt("Please choose 'Rock', 'Paper', or 'Scissors'!").toLowerCase();
+console.log(playerSelection);
 
 //COMPUTER CHOICE
 
@@ -28,36 +28,76 @@ function getComputerChoice () {
             break;
         case 3:
                 return 'scissors';
-                break;
-
     }
 }
 
 const computerSelection = getComputerChoice();
-
 console.log(computerSelection);
-
 
 //PLAY ROUND
 
 //Evaluate the user choice against the computer choice
 //If playerSelection is 'rock' and computerSelection is
 //    'rock'     --> tie
-//    'paper'    --> loss
+//    'paper'    --> lose
 //    'scissors' --> win
 //If playerSelection is 'paper' and computerSelection is
 //    'rock'     --> win
 //    'paper'    --> tie
-//    'scissors' --> loss
+//    'scissors' --> lose
 ////If playerSelection is 'scissors' and computerSelection is
-//    'rock'     --> loss
+//    'rock'     --> lose
 //    'paper'    --> win
 //    'scissors' --> tie
-//Save string of 'win', 'loss', or 'tie' in variable roundResult 
+//Save string of 'win', 'lose', or 'tie' in variable roundResult 
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === 'rock') {
+        switch (computerSelection) {
+            case 'rock': 
+                return 'tie';
+                break;
+            case 'paper':
+                return 'lose';
+                break;
+            case 'scissors':
+                return 'win';
+        };
+    } else if (playerSelection === 'paper') {
+        switch (computerSelection) {
+            case 'rock': 
+                return 'win';
+                break;
+            case 'paper':
+                return 'tie';
+                break;
+            case 'scissors':
+                return 'lose';
+        };       
+    } else if (playerSelection === 'scissors') {
+        switch (computerSelection) {
+            case 'rock': 
+                return 'lose';
+                break;
+            case 'paper':
+                return 'win';
+                break;
+            case 'scissors':
+                return 'tie';
+        };      
+    } else {
+            return 'error';
+        }
+    }
+
+const roundResult = playRound(playerSelection, computerSelection);
+console.log(roundResult);
+
+
 
 //OUTPUT
 
-//Alert message according to win, loss, or tie 
+//Alert message according to win, lose, or tie 
 //Remember to use template literals to include player and computer choices
 //If result was a tie, display message 'Let's try again!'
 //Prompt user to try again.
