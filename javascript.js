f69c668c89c3ffd22650100da800719201b29224
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2024-04-25 12:39:21 +0200

    Add click event to player buttons
    Buttons highlight when clicked and the name value of the clicked button
    is saved to the playerChoice variable.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
+//Save the player's choice in a variable and highlight the chosen button
 
+let playerChoice = "";
+
+const playerBtns = document.querySelectorAll("#player-btns button");
+
+playerBtns.forEach((playerBtn) =>
+  playerBtn.addEventListener("click", function (e) {
+    playerBtns.forEach((playerBtn) => playerBtn.classList.remove("active"));
+    this.classList.add("active");
+    playerChoice = playerBtn.getAttribute("name");
+    return playerChoice;
+  })
+);
