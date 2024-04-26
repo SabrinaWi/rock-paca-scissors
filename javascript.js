4eca19723e00c491a40b3f49587bd4998b4f73a4
Sabrina <sabrinawilliamsde@gmail.comgit config --global color.ui autogit config --get user.namegit config --global user.email sabrinawilliamsde@gmail.com>
2024-04-26 18:34:52 +0200

    Add SoC to player side, add computer and game logic
    - Apply separation of concern to player interaction
    - Add computer choice logic
    - Add round function
    - Structure code more clearly with comments and order of functions

--- javascript.js
+++ javascript.js
@@ -1,14 +1,61 @@
-//Save the player's choice in a variable and highlight the chosen button
-
 let playerChoice = "";
+let computerChoice = "";
 
 const playerBtns = document.querySelectorAll("#player-btns button");
+const computerBtns = document.querySelectorAll("#computer-btns button");
 
-playerBtns.forEach((playerBtn) =>
-  playerBtn.addEventListener("click", function (e) {
-    playerBtns.forEach((playerBtn) => playerBtn.classList.remove("active"));
-    this.classList.add("active");
-    playerChoice = playerBtn.getAttribute("name");
-    return playerChoice;
-  })
-);
+//Get player choice
+
+function getPlayerChoice(playerBtn) {
+  playerChoice = playerBtn.getAttribute("name");
+}
+
+//Highlight player choice
+
+function highlightPlayerChoice(playerBtn) {
+  playerBtns.forEach((playerBtn) => playerBtn.classList.remove("active"));
+  playerBtn.classList.add("active");
+}
+
+//Generate computer choice
+
+function getComputerChoice() {
+  const random = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+  switch (random) {
+    case 1:
+      computerChoice = "rock-btn";
+      break;
+    case 2:
+      computerChoice = "paca-btn";
+      break;
+    case 3:
+      computerChoice = "scissors-btn";
+      break;
+  }
+}
+
+//Highlight computer choice
+
+function highlightComputerChoice(computerChoice) {
+  computerBtns.forEach((computerBtn) => computerBtn.classList.remove("active"));
+  computerBtn = document.querySelector(
+    `#computer-btns button[name="${computerChoice}"]`
+  );
+  computerBtn.classList.add("active");
+}
+
+//Main game logic for one round
+
+function playRound() {
+  getPlayerChoice(this);
+  highlightPlayerChoice(this);
+  getComputerChoice();
+  highlightComputerChoice(computerChoice);
+  console.log(playerChoice, computerChoice);
+}
+
+//Event handler to start a round
+
+playerBtns.forEach((playerBtn) => {
+  playerBtn.addEventListener("click", playRound);
+});
